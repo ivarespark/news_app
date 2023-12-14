80b9af93130515027c527049f7b501a1d0a9d1e2
Ivan Espinosa <ivarespar@gmail.com>
2023-12-14 00:03:05 -0500

    [main] API y Restrofit
    Creacion de interface NewsAPI, intancia de Retrofit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "4b44a633d79945d2831c7b0ff64cfdf7"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
